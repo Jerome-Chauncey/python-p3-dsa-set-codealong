933bb54e01ec38c1aaef3cebcf64a689471b150d
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-05-24 23:45:08 +0300

    implements set data structure

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,43 @@
 class MySet:
+    def __init__(self, enumerable = []):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
 
-    pass
+
+    def has(self, value):
+        return value in self.dictionary
+    
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+    
+    def delete(self,value):
+        self.dictionary.pop(value, None)
+        return self
+    
+    def size(self):
+        return len(self.dictionary)
+    
+    def __repr__(self):
+        values = list(self.dictionary.keys())
+        return f"MySet({values})"
+    
+my_set = MySet([1,2,3])
+
+print(my_set.has(2))
+
+my_set.add(4)
+my_set.add(5)
+my_set.delete(5)
+print(my_set.size())
+
+print(my_set)
+
+
+
+
+
+
+
+
